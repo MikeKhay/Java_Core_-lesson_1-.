9f57ca7a5606d74960ce202e150006e274726709
MikeKhay <khajmike@gmail.com>
2020-02-28 11:07:31 +0200

    HomeWork basics04 MikeKhay

---
+++ logos-example-basics-04/src/WhileLoop.java
@@ -0,0 +1,19 @@
+public class WhileLoop {
+
+    public static void main(String[] args) {
+
+        int c = 1;
+        do {
+            System.out.println("count up " + c);
+            c++;
+
+        } while (c < 5);
+
+
+        int i = 10;
+        while (c > 0) {
+            System.out.println("count down " + i);
+            c--;
+        }
+    }
+}
---
+++ src/Integralelements2.java
@@ -0,0 +1,10 @@
+public class Integralelements2 {
+    public static void main(String[] args) {
+        System.out.printf("Inherent elements of the sequence ");
+
+        for (int a = 90; a > (-1); a = a - 5) {
+            System.out.printf(", " + a);
+        }
+        System.out.printf(".");
+    }
+}
